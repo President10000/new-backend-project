823723768d1bde39c5dc4a9a9b52ce7dbd7b4825
vishal patel <vishallovevap@gmail.com>
2024-01-22 16:04:28 +0530

    cloudinary uploder file added

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import app from "./app.js";
 dotenv.config({ path: "./env" });
 
 connectDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.log("Server error", error);
-      throw error;
-    });
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Server error", error);
+            throw error;
+        });
 
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(` Server is running on port : ${process.env.PORT}`);
-    });
-    app.on("listening", () => {
-      console.log("Server is running");
-    });
-  })
-  .catch((err) => console.log("MongoDB connection failed!!", err));
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running on port : ${process.env.PORT}`);
+        });
+        app.on("listening", () => {
+            console.log("Server is running");
+        });
+    })
+    .catch((err) => console.log("MongoDB connection failed!!", err));
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -3,48 +3,48 @@ import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
-  {
-    videoFile: {
-      type: String,
-      required: [true, "Video is required"],
-    },
+    {
+        videoFile: {
+            type: String,
+            required: [true, "Video is required"],
+        },
 
-    thumbnail: {
-      type: String,
-      required: [true, "Thumbnail is required"],
-    },
+        thumbnail: {
+            type: String,
+            required: [true, "Thumbnail is required"],
+        },
 
-    title: {
-      type: String,
-      required: [true, "Title is required"],
-    },
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+        },
 
-    description: {
-      type: String,
-      required: [true, "Description is required"],
-    },
+        description: {
+            type: String,
+            required: [true, "Description is required"],
+        },
 
-    duration: {
-      type: Number, // get from cloudinary
-      required: [true, "Duration is required"],
-    },
+        duration: {
+            type: Number, // get from cloudinary
+            required: [true, "Duration is required"],
+        },
 
-    views: {
-      type: Number,
-      default: 0,
-    },
+        views: {
+            type: Number,
+            default: 0,
+        },
 
-    isPublic: {
-      type: Boolean,
-      default: true,
-    },
+        isPublic: {
+            type: Boolean,
+            default: true,
+        },
 
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 videoSchema.plugin(mongooseAggregatePaginate);
